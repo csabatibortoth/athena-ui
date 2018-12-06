8b710c1119120af0687d4b4a0343b106b966cb1c
Tóth Csaba <csaba.tibor.toth@outlook.com>
2018-12-06 11:05:23 +0100

    Added basic menu generation

--- src/libs/config/config.js
+++ src/libs/config/config.js
@@ -1,8 +1,10 @@
 config = {
     container_class: "container",
     header_class: "header-container",
+    header_menu_class: "header-menu menu",
     wrapper_class: "wrapper-container",
     footer_class: "footer-container",
+    footer_menu_class: "footer-menu menu",
 
     bordered: true,
 }
--- src/libs/hephaestus/LayoutGenerator/basicLayoutGenerator.js
+++ src/libs/hephaestus/LayoutGenerator/basicLayoutGenerator.js
@@ -9,6 +9,9 @@ function generateBasicLayout() {
     generateHeader();
     generateWrapper();
     generateFooter();
+
+    generateHeaderMenu();
+    generateFooterMenu();
 }
 
 function generateContainer() {
@@ -22,6 +25,8 @@ function generateContainer() {
     });
 }
 
+/* HEADER ELEMENT GENERATION */
+
 function generateHeader() {
     var header_class = config.header_class;
     if (config.bordered) header_class += " default-cointainer-bordered";
@@ -33,6 +38,24 @@ function generateHeader() {
         text: "header"
     });
 }
+
+function generateHeaderMenu() {
+    header_menu_class = config.header_menu_class;
+    if (config.bordered) header_menu_class += " default-cointainer-bordered";
+    HTMLControlGenerator({
+        inputType: "ul",
+        parent: "header",
+        id: "header_menu",
+        class: header_menu_class,
+        items: [
+            { id: "item-1", class: "header-menu-item-class", text: "item-1" },
+            { id: "item-2", class: "header-menu-item-class", text: "item-2" },
+            { id: "item-3", class: "header-menu-item-class", text: "item-3" }
+        ],
+    });
+}
+
+/* WRAPPER ELEMENT GENERATION */
 
 function generateWrapper() {
     var wrapper_class = config.wrapper_class;
@@ -46,6 +69,8 @@ function generateWrapper() {
     });
 }
 
+/* FOOTER ELEMENT GENERATION */
+
 function generateFooter() {
     var footer_class = config.footer_class;
     if (config.bordered) footer_class += " default-cointainer-bordered";
@@ -55,5 +80,21 @@ function generateFooter() {
         id: "footer",
         class: footer_class,
         text: "footer"
+    });
+}
+
+function generateFooterMenu() {
+    footer_menu_class = config.footer_menu_class;
+    if (config.bordered) footer_menu_class += " default-cointainer-bordered";
+    HTMLControlGenerator({
+        inputType: "ul",
+        parent: "footer",
+        id: "footer_menu",
+        class: footer_menu_class,
+        items: [
+            { id: "item-1", class: "footer-menu-item-class", text: "item-1" },
+            { id: "item-2", class: "footer-menu-item-class", text: "item-2" },
+            { id: "item-3", class: "footer-menu-item-class", text: "item-3" }
+        ],
     });
 }
