1db6b618a7c3189946b0f99f09a78e24e0010f6a
Tóth Csaba <csaba.tibor.toth@outlook.com>
2019-01-16 15:59:15 +0100

    outside modal click disabled

--- src/libs/hephaestus/modalGenerator/modalGenerator.js
+++ src/libs/hephaestus/modalGenerator/modalGenerator.js
@@ -18,9 +18,9 @@ span.onclick = function() {
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+/*window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+}*/
 
